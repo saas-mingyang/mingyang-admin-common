d8896b800f47440bad1f802e965b073444487913
huanglei19951029 <81128461+huanglei19951029@users.noreply.github.com>
2025-12-06 14:24:21 +0800

    refactor(utils): 重构转换工具函数
    - 移除字符串与整数切片相互转换函数
    - 移除 Protobuf uint8/uint32 转换辅助函数
    - 新增时间戳转时间指针的辅助

--- utils/convert/convert_utils.go
+++ utils/convert/convert_utils.go
@@ -1,47 +1,12 @@
 package convert
 
-import "strconv"
+import "time"
 
-// StringSliceToUint64Slice 将字符串切片转换为 uint64 切片
-func StringSliceToUint64Slice(strSlice []string) []uint64 {
-	result := make([]uint64, 0, len(strSlice))
-
-	for _, str := range strSlice {
-		val, err := strconv.ParseUint(str, 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		result = append(result, val)
-	}
-
-	return result
-}
-
-// Uint64SliceToStringSlice 将 uint64 切片转换为字符串切片
-func Uint64SliceToStringSlice(uintSlice []uint64) []string {
-	result := make([]string, len(uintSlice))
-
-	for i, val := range uintSlice {
-		result[i] = strconv.FormatUint(val, 10)
-	}
-
-	return result
-}
-
-// GetUint8FromProto 从 protobuf 中获取 uint8
-func GetUint8FromProto(protoField *uint32) *uint8 {
-	if protoField == nil {
+// TimePtrFromUnix 辅助函数：时间戳转指针，为0返回nil
+func TimePtrFromUnix(unix int64) *time.Time {
+	if unix == 0 {
 		return nil
 	}
-	val := uint8(*protoField)
-	return &val
-}
-
-// GetUint32FromProto 从 protobuf 中获取 uint32
-func GetUint32FromProto(protoField *uint8) *uint32 {
-	if protoField == nil {
-		return nil
-	}
-	val := uint32(*protoField)
-	return &val
+	t := time.Unix(unix, 0)
+	return &t
 }
