d97483631a187878110dbb4a03dc85fb61f17142
huanglei19951029 <81128461+huanglei19951029@users.noreply.github.com>
2026-03-23 14:08:32 +0800

    feat(enum): 添加日期时间格式常量
    - 添加 DateFormat 常量用于日期格式化
    - 添加 DateTimeFormat 常量用于日期时间格式化
    - 添加 TimeFormat 常量用于时间格式化

--- enum/common/common.go
+++ enum/common/common.go
@@ -9,4 +9,7 @@ const (
 	DefaultParentId uint64 = 1000000
 	// DefaultInvalidRoleId used to judge whether the token belongs to core, if it is DefaultInvalidRoleId, it belongs to mms
 	DefaultInvalidRoleId uint64 = 1000000
+	DateFormat                  = "2006-01-02"
+	DateTimeFormat              = "2006-01-02 15:04:05"
+	TimeFormat                  = "15:04:05"
 )
